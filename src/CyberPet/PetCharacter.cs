607f9ed44425ca177e215bc5337ff02e1f435d37
liam wheeler <liam_wheeler@hotmail.com>
2019-05-15 12:12:54 +0100

    initial commit

---
+++ src/CyberPet/PetCharacter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberPet
+{
+    public class PetCharacter
+    {
+        public string name;
+        public int energy = 10;
+        public int hunger = 0;
+        public int happy = 6;
+
+        public void Feed()
+        {
+            if (hunger == 0)
+            {
+                Console.WriteLine(name + " is not hungry right now");
+            }
+            else if (hunger <= 9)
+            {
+                hunger -= 2;
+                Console.WriteLine(name + " has been fed, hunger is now " + hunger);
+            }
+
+        }
+
+        public void Play()
+        {
+            if (energy == 0)
+            {
+                Console.WriteLine(name + " is too tired to play right now.");
+            }
+            else if (happy == 10)
+            {
+                energy -= 2;
+                hunger += 2;
+                Console.WriteLine(name + " is as happy as can be!");
+            }
+            else if (hunger == 10)
+            {
+                Console.WriteLine(name + " is too hungry to play right now!");
+            }
+            else
+            {
+                energy -= 2;
+                hunger += 2;
+                happy += 2;
+
+            }
+        }
+
+        public void Sleep()
+        {
+            if (energy == 10)
+            {
+                Console.WriteLine(name + " is not tired right now");
+            }
+            else
+            {
+                energy = 10;
+                Console.WriteLine(name + " had a great sleep and is ready to play some more");
+            }
+        }
+
+
+
+    }
+}
---
+++ src/CyberPet/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+
+namespace CyberPet
+{
+    public class Program
+    {
+        public static PetCharacter myPet = new PetCharacter();
+        private static bool invalidName = true;
+
+        static void Main(string[] args)
+        {
+            while (invalidName == true)
+            {
+                try
+                {
+                    Console.WriteLine("What would you like to name your new cyberpet.");
+                    string userName = Console.ReadLine();
+                    if (!string.IsNullOrWhiteSpace(userName))
+                    {
+                        myPet.name = userName.First().ToString().ToUpper() + userName.Substring(1);
+                        Console.WriteLine("Congratulations, your new pet is called " + myPet.name);
+                        invalidName = false;
+                    }
+                    else throw new ArgumentNullException();
+                }
+                catch (ArgumentNullException)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Your cyberpets name cannot be null or whitespace");
+                    Console.ResetColor();
+                    
+                }
+            }
+
+
+            while (true)
+            {
+                
+
+                
+
+
+                    Console.WriteLine("Feed or Play with " + myPet.name + " or let them sleep");
+                    Console.WriteLine(myPet.name + "'s stats" +"\r\nEnergy: " + myPet.energy
+                                    + "\r\nHunger: " + myPet.hunger + "\r\nHappiness: " + myPet.happy);
+                    Console.WriteLine("Or press 'q' to quit");
+                    var input = Console.ReadLine();
+
+                try
+                {     
+                    if (input == "q")
+                    {
+                        break;
+                    }
+                    else if (myPet.hunger == 10)
+                    {
+                        Console.WriteLine(myPet.name + " has ran away in search of food");
+                        break;
+                    }
+                    else if (input == "play")
+                    {
+                        myPet.Play();
+                    }
+                    else if (input == "feed")
+                    {
+                        myPet.Feed();
+
+                    }
+                    else if (input == "sleep")
+                    {
+                        myPet.Sleep();
+                    }
+                    else throw new ArgumentException();
+                }
+
+
+
+                catch (ArgumentException)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Unexpected input, try either 'feed', 'play' or 'sleep'.");
+                    Console.ResetColor();
+                }
+
+            }
+                Console.ReadLine();          
+        }    
+    }
+}
